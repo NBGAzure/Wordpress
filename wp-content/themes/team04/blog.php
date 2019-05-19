66220e36e6aee1c85509ae2172fce8da08b0e0d2
VickyDesai <vickydesai8002@gmail.com>
2019-05-19 13:59:38 +1000

    Blog page added with additional css
    Co-Authored-By: vipuldeep <vipuldeep@users.noreply.github.com>

---
+++ wp-content/themes/team04/blog.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+        crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Roboto+Slab" rel="stylesheet">
+    <link rel="stylesheet" href="style/style.css">
+
+    <title>Ian Kirkwood</title>
+</head>
+<body>
+  <div class="sidenav">
+    <a href="#">name</a>
+    <a href="#">name</a>
+    <a href="#">name</a>
+    <a href="#">Blog name</a>
+  </div>
+
+  <div class="sidenavsidecontext">
+    <div class="container-fluid text-center py-5">
+    <a href="#"><h2 class="section-heading">All Blogs</h2></a>
+    </div>
+
+              <section>
+
+                <div class="card mb-3">
+                    <div class="card-description">
+                <div class="container-fluid">
+                <div class="mt-3 px-2">
+                  <h3 class="section-heading ml-2">About</h3>
+                <p>
+                <div class="card-image">
+                <a href="#"><img src="img/1.jpg" width="400" height="400" style="max-width: 80%; max-height: 70%; float: right; padding: 16px;"></a>
+                <p id="texts">Few words for blog description of what he likes sharing</p>
+                                    <p id="texts">nkvnlernvlkernvlknerklvnerlknvklernvklnelknfklrenklfnreflknreklfnerlknflkrenflkrenflkernflkrenflkrenflkrenflkrenfklrenfrelknflreknfrle</span>
+
+
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+
+    <div class="card mb-3">
+        <div class="card-description">
+    <div class="container-fluid">
+    <div class="mt-3 px-2">
+    <h3 class="section-heading ml-2">Blogs</h3>
+    <p>
+    <div class="card-image">
+    <a href="blogpost.html"><img src="img/1.jpg" width="400" height="300" style="max-width: 80%; height: auto; float: right; padding: 16px;"></a>
+    <p id="texts">Few words for blog description of what he likes sharing</p>
+                        <p id="texts">nkvnlernvlkernvlknerklvnerlknvklernvklnelknfklrenklfnreflknreklfnerlknflkrenflkrenflkernflkrenflkrenflkrenflkrenfklrenfrelknflreknfrlenflnfrleknflernflkenrlkfnelrknflerknflenflekfnlernfrlenlnflknerl! </p>
+                        <p id="texts">Situated in the city centre with extensive views of the river and the city, City Backpackers HQ is the place to be! In walking distance from the best shops, bars and restaurants and new entertainment precincts;</p>
+                        <div class="text-center">
+                        <a href="#"><button type="button" class="btn btn-outline-danger">Read more</button></a><br><br>
+                      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+                </section>
+
+
+
+
+                <section>
+                  <div class="card mb-3">
+                      <div class="card-description">
+                  <div class="container-fluid">
+                  <div class="mt-3 px-2">
+                  <h3 class="text-left">Info</h3>
+                  <p>
+                  <div class="card-image">
+                  <a href="blogpost.html"><img src="img/1.jpg" width="400" height="300" style="max-width: 80%; height: auto; float: right; padding: 16px;"></a>
+                  <p id="texts">Few words for blog description of what he likes sharing</p>
+                                      <p id="texts">nkvnlernvlkernvlknerklvnerlknvklernvklnelknfklrenklfnreflknreklfnerlknflkrenflkrenflkernflkrenflkrenflkrenflkrenfklrenfrelknflreknfrlenflnfrleknflernflkenrlkfnelrknflerknflenflekfnlernfrlenlnflknerl! </span>
+                                      <p id="texts">Situated in the city centre with extensive views of the river and the city, City Backpackers HQ is the place to be! In walking distance from the best shops, bars and restaurants and new entertainment precincts;</p>
+                                      <p id="texts">making it the perfect place to catch a movie, have a drink with friends or buy those much needed essentials.</p>
+                                      <div class="text-center">
+                                      <a href="#"><button type="button" class="btn btn-outline-danger">Read more</button></a><br><br>
+                                    </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+
+                  <div class="card mb-3">
+                      <div class="card-description">
+                  <div class="container-fluid">
+                  <div class="mt-3 px-2">
+                  <h3 class="text-left">Blogs</h3>
+                  <p>
+                  <div class="card-image">
+                  <a href="blogpost.html"><img src="img/1.jpg" width="400" height="300" style="max-width: 80%; height: auto; float: right; padding: 16px;"></a>
+                  <p id="texts">Few words for blog description of what he likes sharing</p>
+                                      <p id="texts">nkvnlernvlkernvlknerklvnerlknvklernvklnelknfklrenklfnreflknreklfnerlknflkrenflkrenflkernflkrenflkrenflkrenflkrenfklrenfrelknflreknfrlenflnfrleknflernflkenrlkfnelrknflerknflenflekfnlernfrlenlnflknerl! </p>
+                                      <p id="texts">Situated in the city centre with extensive views of the river and the city, City Backpackers HQ is the place to be! In walking distance from the best shops, bars and restaurants and new entertainment precincts;</p>
+                                      <p id="texts"> making it the perfect place to catch a movie, have a drink with friends or buy those much needed essentials.</p>
+                                      <div class="text-center">
+                                      <a href="#"><button type="button" class="btn btn-outline-danger">Read more</button></a><br><br>
+                                    </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+                  </div>
+
+            </section>
+  </div>
+
+</body>
+
+</html>
